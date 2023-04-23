2c1c7da50176c0cdcc8578570286ca3d4f2e9b95
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-04-23 19:42:02 +0530

    class based component

---
+++ src/Components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NewItem from './NewItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        this is news component
+        <NewItem/>
+      </div>
+    )
+  }
+}
+
+export default News
