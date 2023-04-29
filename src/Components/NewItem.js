f04d3a65da2e5d494b11f0bcdfce0671460f08f1
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-04-29 21:51:56 +0530

    added pre/next buttons

--- src/Components/NewItem.js
+++ src/Components/NewItem.js
@@ -6,15 +6,14 @@ export class NewItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width:"18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/04/27/1600x900/sun-2224937_1280_1677654423439_1682570104399.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">
              {description}...
             </p>
             {/* use target="_blank" for redirect the that perticular news which we have choosen */}
-             Read more
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">             
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">             
                 Read more
             </a>
           </div>
