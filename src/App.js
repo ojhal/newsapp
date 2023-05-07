52ac53c3ff1c1eda70ea6ff6a462752719e7e10f
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-05-07 21:54:31 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
+import "./App.css";
 
-import './App.css';
-
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar';
-import News from './Components/News';
-
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
       <div>
-       <Navbar/>
-       <News pageSize={8}
-            country="in"
-            category="sports"/>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/">
+              <News pageSize={8} country="in" category="general" />
+            </Route>
+            <Route path="/business">
+              <News pageSize={8} country="in" category="business" />
+            </Route>
+            <Route path="/entertainment">
+              <News pageSize={8} country="in" category="entertainment" />
+            </Route>
+            <Route path="/general">
+              <News pageSize={8} country="in" category="general" />
+            </Route>
+            <Route path="/health">
+              <News pageSize={8} country="in" category="health" />
+            </Route>
+            <Route path="/science">
+              <News pageSize={8} country="in" category="science" />
+            </Route>
+            <Route path="/sports">
+              <News pageSize={8} country="in" category="sports" />
+            </Route>
+            <Route path="/technology">
+              <News pageSize={8} country="in" category="technology" />
+            </Route>
+          </Switch>
+        </Router>
       </div>
-    )
+    );
   }
 }
-
