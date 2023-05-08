7d53548b5f42dbd7f79f213384effa5971f0d9db
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-05-08 22:32:44 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -11,30 +11,14 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Switch>
-            <Route path="/">
-              <News pageSize={8} country="in" category="general" />
-            </Route>
-            <Route path="/business">
-              <News pageSize={8} country="in" category="business" />
-            </Route>
-            <Route path="/entertainment">
-              <News pageSize={8} country="in" category="entertainment" />
-            </Route>
-            <Route path="/general">
-              <News pageSize={8} country="in" category="general" />
-            </Route>
-            <Route path="/health">
-              <News pageSize={8} country="in" category="health" />
-            </Route>
-            <Route path="/science">
-              <News pageSize={8} country="in" category="science" />
-            </Route>
-            <Route path="/sports">
-              <News pageSize={8} country="in" category="sports" />
-            </Route>
-            <Route path="/technology">
-              <News pageSize={8} country="in" category="technology" />
-            </Route>
+            <Route exact path="/"><News key="general" pageSize={8} country="in" category="general" /></Route>
+            <Route exact path="/business"><News key="business" pageSize={8} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News key="entertainment" pageSize={8} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News key="general" pageSize={8} country="in" category="general" /></Route>
+            <Route  exact path="/health"><News key="health" pageSize={8} country="in" category="health" /></Route>
+            <Route exact path="/science"><News key="science" pageSize={8} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News key="sports" pageSize={8} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News key="technology" pageSize={8} country="in" category="technology" /></Route>
           </Switch>
         </Router>
       </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,9 +18,9 @@ export class Navbar extends Component {
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
             </li>
+            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
