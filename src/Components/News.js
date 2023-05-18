0f4f108e64f615bdfc7f117fe6c52498dc966ebb
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-05-18 23:22:15 +0530

    add length

--- src/Components/News.js
+++ src/Components/News.js
@@ -59,7 +59,7 @@ const  News = (props) => {
         </h1>
         {loading && <Spinner />}
         <InfiniteScroll
-          dataLength={articles}
+          dataLength={articles.length}
           next={fetchMoreData}
           hasMore={articles.length !== totalResults}
           loader={<Spinner />}
